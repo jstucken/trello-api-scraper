3646b50c835da9a97349bf8fad15c9f530ba06b0
Jonathan Stucken <jonathan.stucken@swsphn.com.au>
2023-06-08 15:46:57 +1000

    Shorten comments

--- get_trello_cards.py
+++ get_trello_cards.py
@@ -1,19 +1,6 @@
-# This script gets the number of cards for a particular Trello board
-# uses CURL requests to pull data using the Trello API
-# see https://stateful.com/blog/trello-api-examples
-# https://developer.atlassian.com/cloud/trello/guides/rest-api/api-introduction/
-#
-# To automate this script with a CRON job, it's advised to store your Trello API creds
-# in a secure folder e.g. /root/trello_credentials containing the following variables:
-# TRELLO_API_KEY=xxxxxxxxxxxx
-# TRELLO_API_TOKEN=xxxxxxxxxxxx
-# TRELLO_BOARD_ID=xxxxxxxxxxxx
-# Your Trello board id can be found by putting '.json' at end of the Trello board URL
-# in your browser.
-# Then run this script as root/sudo user so the script can pull in the
-# trello_credentials correctly.
-# e.g.
-# sudo poetry run python get_trello_cards.py
+# This script gets the number of cards for a particular Trello board uses CURL requests
+# to pull data using the Trello API.
+# See the README.md to setup TRELLO_CREDENTIALS_PATH and credentials correctly.
 #
 # Created by jstucken 9/2/2023
 
